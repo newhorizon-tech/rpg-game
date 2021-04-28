c749b4b1ce8e9513e47649f656e3122737934ee4
newhorizon-tech <alaukikinbox@gmail.com>
2021-04-28 22:11:49 +0530

    Add waiting period

--- src/scenes/scene-1.js
+++ src/scenes/scene-1.js
@@ -1,4 +1,8 @@
 export class Scene1 extends Phaser.Scene {
+  const sleep = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
   constructor() {
     super("Scene1");
   }
@@ -7,11 +11,14 @@ export class Scene1 extends Phaser.Scene {
     this.load.image('boat', 'assets/boat.jpg');
   }
 
-  create() {
+
+  async create() {
     this.add.text(100, 100, 'Scene One!', {
       fill: '#0f0'
     });
     this.add.image(400, 300, 'boat');
+    await sleep(8000);
+    
     this.scene.start("Scene2")
   }
 }
